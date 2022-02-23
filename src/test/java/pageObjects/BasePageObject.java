04aa00eada9d0158b8e25e9e5275e37edced8024
Hega <hegedusrendszer@gmail.com>
2022-02-23 11:21:06 +0100

    New tests are added and implemented.

--- src/test/java/pageObjects/BasePageObject.java
+++ src/test/java/pageObjects/BasePageObject.java
@@ -1,11 +1,17 @@
 package pageObjects;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
 import java.util.List;
 
 
@@ -19,11 +25,18 @@ public class BasePageObject {
     }
 
     void waitForElement(WebElement element) {
-        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(element));
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(element));
     }
     void waitForElements(List<WebElement> list){
-        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfAllElements(list));
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfAllElements(list));
     }
+
+
+    public void takeScreenshot() throws IOException {
+        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFileToDirectory(file, new File("src\\test\\resources\\screenshots"));
+    }
+
 
 
 }
